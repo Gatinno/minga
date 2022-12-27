3d859bfb8d2682bfab64b023304360cddc1c6638
laptinho <camsanbar@gmail.com>
2022-12-27 11:54:29 -0300

    add react-router & create home view

---
+++ src/views/Home.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Header } from "../layout";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main id='main'>
+				<div className='slideshow-container'>
+					<div className='mySlides fade'>
+						<div className='numbertext'>1 / 3</div>
+						<img
+							src='https://occ-0-300-990.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUhJMx1ZGjVQ6PrRpwkawoR_l5TWtESU4TTngpctPd5SyCN91X2TNF6IAKmXJpv9oISMu_brPcvDToTLAYabaOykcOfpmLeyCDvF.jpg?r=60a'
+              style={{width:'100%'}}
+              alt=''
+						/>
+						<div className='text'>Caption Text</div>
+					</div>
+
+					<button className='prev' onClick={()=> 1}>
+						&#10094;
+					</button>
+					<button className='next' onClick={()=> 1}>
+						&#10095;
+					</button>
+				</div>
+				<br />
+
+				<div style={{textAlign:'center'}}>
+					<span className='dot' onClick={()=> 1}></span>
+					<span className='dot' onClick={()=> 1}></span>
+					<span className='dot' onClick={()=> 1}></span>
+				</div>
+			</main>
+			<footer>
+				<div className='section-footer'>
+					<img className='logo' src='./logo.png' alt='logo' />
+					<p>Comics</p>
+				</div>
+				<p>©2022 Comic App.</p>
+				<div className='pages'>
+					<h3>Pages</h3>
+					<a href='#'>Comics</a>
+				</div>
+			</footer>
+    </>
+  )
+}
+export default Home
